858fe33c791ad028dd4e934484d55e80e1b20210
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2020-03-15 22:13:18 -0500

    Initial commit

---
+++ src/main/java/com/jeeps/covidopenknowledgeutplapi/CovidOpenknowledgeUtplApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeeps.covidopenknowledgeutplapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidOpenknowledgeUtplApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidOpenknowledgeUtplApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeeps/covidopenknowledgeutplapi/CovidOpenknowledgeUtplApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeeps.covidopenknowledgeutplapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidOpenknowledgeUtplApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
