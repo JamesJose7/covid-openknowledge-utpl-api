864bab37caa6c2820302f235bc257f072bc682be
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2020-03-16 12:52:02 -0500

    Fixed context path in swagger docs

--- src/main/java/com/jeeps/covidopenknowledgeutplapi/core/SpringFoxConfig.java
+++ src/main/java/com/jeeps/covidopenknowledgeutplapi/core/SpringFoxConfig.java
@@ -1,8 +1,10 @@
 package com.jeeps.covidopenknowledgeutplapi.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,12 +16,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @EnableSwagger2WebMvc
 @Import(SpringDataRestConfiguration.class)
 public class SpringFoxConfig {
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .paths(PathSelectors.ant("/covidOpenKnowledge/api/v1/**"))
+                .build()
+                .pathProvider(new PathProvider() {
+                    @Override
+                    public String getOperationPath(String operationPath) {
+                        return operationPath.replace(contextPath, "");
+                    }
+
+                    @Override
+                    public String getResourceListingPath(String groupName, String apiDeclaration) {
+                        return null;
+                    }
+                });
     }
 }
